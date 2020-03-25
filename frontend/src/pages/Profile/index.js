5009be7afba33223e0ff26f27c82d919cd684f46
Jaimerson <jaimersonsantos@ufam.edu.br>
2020-03-25 16:32:59 -0400

    features logout complete

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import api from "../../services/api";
@@ -9,6 +9,9 @@ import logoImg from "../../assets/logo.svg";
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
+
   const ongId = localStorage.getItem("ongId");
   const ongName = localStorage.getItem("ongName");
 
@@ -34,6 +37,12 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push("/");
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -44,7 +53,7 @@ export default function Profile() {
           Cadastrar novo caso
         </Link>
 
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
